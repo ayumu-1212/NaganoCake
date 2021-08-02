6055cd7bbd57ca219cb016f2505da27269b3c57f
ayumu-1212 <abe.ayumu.infratop@gmail.com>
2021-08-02 16:44:39 +0000

    :bookmark: エンドユーザーの商品一覧機能

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,10 @@
 class Public::ItemsController < ApplicationController
+  def index
+    if params[:gid].nil?
+      @items = Item.all
+    else
+      @genre = Genre.find(params[:gid])
+      @items = @genre.items
+    end
+  end
 end
