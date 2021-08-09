dd9a097f74922a34988af111b3d1b426fe3d9de5
ayumu-1212 <abe.ayumu.infratop@gmail.com>
2021-08-09 13:05:56 +0000

    :art: ボタンのレイアウト調整

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,7 +6,7 @@ class Admin::OrdersController < ApplicationController
     else
       end_user = EndUser.find(params[:end_user_id])
       @orders = Order.where(end_user_id: end_user.id)
-      flash[:info] = "こちらは#{end_user.last_name + end_user.first_name}さんの注文履歴です。"
+      flash.now[:info] = "こちらは#{end_user.last_name + end_user.first_name}さんの注文履歴です。"
     end
   end
 
