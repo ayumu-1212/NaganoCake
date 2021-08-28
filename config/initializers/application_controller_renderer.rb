ddecf280b5539aaa7176877a0c7ab3794e6526d0
ayumu-1212 <abe.ayumu.infratop@gmail.com>
2021-08-28 06:55:07 +0000

    :bug: refileのsecretkeyを設定

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5ef2c96e5add4fec3ee30312912b8254bdb2fb2ce6a3144c732d12b9b4578c5bd73b5f1b0845f65bc8b724bf4fb34107538c0c7286443e75274dbd7c6d07fd1f'
